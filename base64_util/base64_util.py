f935ba84aabca9e52e50a98d673d9c11a0b64da3
fieliapm <fieliapm@gmail.com>
2016-04-28 20:42:18 +0800

    add base64_util

---
+++ base64_util/base64_util.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import base64
+
+
+class Base64Codec(object):
+    def __init__(self, encoder, decoder):
+        self.__encoder = encoder
+        self.__decoder = decoder
+
+    def b64encode_bytes(self, bytes_):
+        return self.__encoder(bytes_).decode('utf-8')
+
+    def b64decode_bytes(self, encoded_bytes):
+        return self.__decoder(encoded_bytes.encode('utf-8'))
+
+    def b64encode_bytes_without_padding(self, bytes_):
+        return self.b64encode_bytes(bytes_).rstrip('=')
+
+    def b64decode_bytes_without_padding(self, encoded_bytes):
+        base64_padding = '='*((-(len(encoded_bytes)&0x3))&0x3)
+        return self.b64decode_bytes(encoded_bytes+base64_padding)
+
+    def b64encode_string(self, string):
+        return self.b64encode_bytes(string.encode('utf-8'))
+
+    def b64decode_string(self, encoded_string):
+        return self.b64decode_bytes(encoded_string).decode('utf-8')
+
+    def b64encode_string_without_padding(self, string):
+        return self.b64encode_bytes_without_padding(string.encode('utf-8'))
+
+    def b64decode_string_without_padding(self, encoded_string):
+        return self.b64decode_bytes_without_padding(encoded_string).decode('utf-8')
+
+
+standard = Base64Codec(base64.standard_b64encode, base64.standard_b64decode)
+urlsafe = Base64Codec(base64.urlsafe_b64encode, base64.urlsafe_b64decode)
+
+
+# test case
+
+import unittest
+
+
+class Base64UtilTestCase(unittest.TestCase):
+    def setUp(self):
+        self.decoded_string = u'歪みねぇな!定岡ウェーブ'
+        self.encoded_string = '5q2q44G/44Gt44GH44GqIeWumuWyoeOCpuOCp+ODvOODlg=='
+        self.encoded_string_without_padding = self.encoded_string.rstrip('=')
+        self.urlsafe_encoded_string = self.encoded_string.replace('+', '-').replace('/', '_')
+        self.urlsafe_encoded_string_without_padding = self.urlsafe_encoded_string.rstrip('=')
+
+    def test_standard_byte(self):
+        self.assertEqual(standard.b64encode_bytes(self.decoded_string.encode('utf-8')), self.encoded_string)
+        self.assertEqual(standard.b64decode_bytes(self.encoded_string), self.decoded_string.encode('utf-8'))
+
+    def test_standard_byte_without_padding(self):
+        self.assertEqual(standard.b64encode_bytes_without_padding(self.decoded_string.encode('utf-8')), self.encoded_string_without_padding)
+        self.assertEqual(standard.b64decode_bytes_without_padding(self.encoded_string_without_padding), self.decoded_string.encode('utf-8'))
+
+    def test_standard_string(self):
+        self.assertEqual(standard.b64encode_string(self.decoded_string), self.encoded_string)
+        self.assertEqual(standard.b64decode_string(self.encoded_string), self.decoded_string)
+
+    def test_standard_string_without_padding(self):
+        self.assertEqual(standard.b64encode_string_without_padding(self.decoded_string), self.encoded_string_without_padding)
+        self.assertEqual(standard.b64decode_string_without_padding(self.encoded_string_without_padding), self.decoded_string)
+
+    def test_urlsafe_byte(self):
+        self.assertEqual(urlsafe.b64encode_bytes(self.decoded_string.encode('utf-8')), self.urlsafe_encoded_string)
+        self.assertEqual(urlsafe.b64decode_bytes(self.urlsafe_encoded_string), self.decoded_string.encode('utf-8'))
+
+    def test_urlsafe_byte_without_padding(self):
+        self.assertEqual(urlsafe.b64encode_bytes_without_padding(self.decoded_string.encode('utf-8')), self.urlsafe_encoded_string_without_padding)
+        self.assertEqual(urlsafe.b64decode_bytes_without_padding(self.urlsafe_encoded_string_without_padding), self.decoded_string.encode('utf-8'))
+
+    def test_urlsafe_string(self):
+        self.assertEqual(urlsafe.b64encode_string(self.decoded_string), self.urlsafe_encoded_string)
+        self.assertEqual(urlsafe.b64decode_string(self.urlsafe_encoded_string), self.decoded_string)
+
+    def test_urlsafe_string_without_padding(self):
+        self.assertEqual(urlsafe.b64encode_string_without_padding(self.decoded_string), self.urlsafe_encoded_string_without_padding)
+        self.assertEqual(urlsafe.b64decode_string_without_padding(self.urlsafe_encoded_string_without_padding), self.decoded_string)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
