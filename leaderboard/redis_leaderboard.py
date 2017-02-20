d1d3bf02c63aa63ec0087f11867d501ce1cc0884
fieliapm <fieliapm@gmail.com>
2017-02-20 19:55:51 +0800

    add GPL warning to redis_leaderboard.py

--- leaderboard/redis_leaderboard.py
+++ leaderboard/redis_leaderboard.py
@@ -2,6 +2,29 @@
 # -*- coding: utf-8 -*-
 
 
+################################################################################
+#
+# redis_leaderboard - read and store leaderboard data with redis database
+#                     compatible with Python 2 & 3
+# Copyright (C) 2016-present Himawari Tachibana <fieliapm@gmail.com>
+#               original source: https://docs.python.org/2/library/csv.html
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+
 import time
 #import sys
 
