33b56501c48c2718806129e8cbceabb0db2eff20
fieliapm <fieliapm@gmail.com>
2020-02-06 18:32:22 +0800

    assign version 0.2.0 to lager

--- lager/setup.py
+++ lager/setup.py
@@ -30,7 +30,7 @@ from distutils.core import setup
 
 
 setup(name='lager',
-    version='0.1.0',
+    version='0.2.0',
     description='an easy tool to access cloud storage service,\nand also, abstract and simplify corresponding API',
     author='Himawari Tachibana',
     author_email='fieliapm@gmail.com',
